939c0a531b62e80a57f6e7c3dc11fd8c7d246373
Alex-Polishchuk <58093667+Alex-Polishchuk@users.noreply.github.com>
2023-03-23 17:59:41 +0000

    Add files via upload

--- premier_league_data.py
+++ premier_league_data.py
@@ -1,23 +1,45 @@
 import pandas as pd
-import csv
 import seaborn as sb
 import sqlite3 as sql
+from sqlite3 import Error
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
-data_pd = pd.read_csv('results.csv')
+
+data_pd = pd.read_csv('results.csv', parse_dates=True)
 data = pd.DataFrame(data_pd)
 
 #print(data.iloc[0,:])
 #print(data.loc[:, ['HomeTeam', 'FTHG', 'FTAG', 'AwayTeam']]) #FORMAT TO HAVE HOME TEAM, HOMEGOAL, AWAYGOAL, AWAYTEAM
 
 #this shows when a team was winning at half time and then lost at half-time
-#print(data.loc[(data.HomeTeam == 'Arsenal') & (data.FTR == 'A') & (data.HTR == 'H')])
+#print(data.loc[(data.AwayTeam == 'Liverpool') & (data.FTR == 'A') & (data.HTR == 'H')])
 
 #print(data.loc[data.FTAG.isin([8])])
-#print(data.loc[data.FTHG.isin([9])])
+#print(data.loc[data.FTHG.isin([6, 7, 8, 9]) & (data.HomeTeam == 'Liverpool')])
+#print(list(data.columns))
 
-sql_data = sql.connect(data)
-cursor = sql_data.cursor()
+#engine = create_engine('sqlite:///prem_data.db', echo=True)
+#data.to_sql('prem_data', con=engine, if_exists='replace', index=False)
+open_connection = sql.connect('C:\\Users\\alexp\\Desktop\\Code\\Data Practice\\prem_data.db')
+cursor = open_connection.cursor()
 
-#pd.read_sql_query(query, sql_data)
+query = """
+    SELECT HomeTeam
+    FROM prem_data
+    WHERE AwayTeam = 'Liverpool' and  FTR = 'H'
+"""
 
-sql_data.close()
+cursor.execute(query)
+results = cursor.fetchall()
+
+print(results)
+
+
+
+###CREATING AN SQL QUERY TO CREATE A DATA frame | 1. create the query and save to dataframe. 2. create a name for the file. 3. save the file and create a plot 
+
+
+#Allows us to close a connection when needed
+cursor.close()
+open_connection.close()
