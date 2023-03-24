351a5a6ea74ad2293791bca3f5a008449a9eabac
Alex-Polishchuk <58093667+Alex-Polishchuk@users.noreply.github.com>
2023-03-24 12:07:27 +0000

    Add files via upload

--- premier_league_data.py
+++ premier_league_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
 import sqlite3 as sql
-from sqlite3 import Error
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
@@ -16,7 +15,7 @@ data = pd.DataFrame(data_pd)
 #print(data.loc[(data.AwayTeam == 'Liverpool') & (data.FTR == 'A') & (data.HTR == 'H')])
 
 #print(data.loc[data.FTAG.isin([8])])
-#print(data.loc[data.FTHG.isin([6, 7, 8, 9]) & (data.HomeTeam == 'Liverpool')])
+#print(data.loc[data.FTHG.isin([5, 6, 7, 8, 9]) & data.FTAG.isin([2, 3, 4, 5, 6, 7])])
 #print(list(data.columns))
 
 #engine = create_engine('sqlite:///prem_data.db', echo=True)
@@ -24,21 +23,64 @@ data = pd.DataFrame(data_pd)
 open_connection = sql.connect('C:\\Users\\alexp\\Desktop\\Code\\Data Practice\\prem_data.db')
 cursor = open_connection.cursor()
 
-query = """
-    SELECT HomeTeam
+# first_query = """
+#     SELECT Season, DateTime, HomeTeam, FTHG, FTAG, AwayTeam
+#     FROM prem_data
+#     WHERE (HomeTeam = 'Liverpool' and AwayTeam = 'Man United') or ((HomeTeam = 'Man United' and AwayTeam = 'Liverpool'))
+#     ORDER BY DateTime
+# """
+# second_query = """
+#     SELECT Season, DateTime, HomeTeam, FTHG, FTAG, AwayTeam
+#     FROM prem_data
+#     WHERE Season = '2020-21'
+#     ORDER BY DateTime
+# """
+
+third_query = """
+    SELECT Season, SUM(HR), SUM(AR)
     FROM prem_data
-    WHERE AwayTeam = 'Liverpool' and  FTR = 'H'
+    GROUP By Season
 """
 
-cursor.execute(query)
-results = cursor.fetchall()
+#FETCHING AND ASSIGNING THE DATA
+# cursor.execute(first_query)
+# cursor.execute(second_query)
+#cursor.execute(third_query)
+# first_results = cursor.fetchmany()
+# second_results = cursor.fetchall()
+#third_results = cursor.fetchall()
 
-print(results)
+#CONVERTING THE SQL DATA TO A PANDA DATAFRAME SO IT CAN BE USED TO VISUALISE ETC
+# first_processed_data = pd.DataFrame(first_results, columns=['Season', 'DateTime', 'Home Team','HG','AG','Away Team'])
+# second_processed_data = pd.DataFrame(second_results, columns=['Season', 'DateTime', 'HomeTeam', 'HG', 'AG', 'AwayTeam'])
+# third_processed_data = pd.DataFrame(third_results, columns = ['Season', 'Yellows', 'Reds'])
+# print(third_processed_data)
+
+# graph_figure = sns.lineplot(data=third_processed_data)
+# graph_figure.figure.savefig('graph.png')
+
+def graph_plotter (queryName, graphColumns, graphOut):
+    cursor.execute(queryName)
+    result = cursor.fetchall()
+
+    #WRITE TO A DATA FRAME
+    processed_data = pd.DataFrame(result, columns = graphColumns)
+
+    #OUTPUT TO A GRAPH
+    graph_figure = sns.lineplot(data=processed_data)
+    graph_figure.figure.savefig(graphOut)
+
+new_query = ['Season', 'Home Red', 'Away Red']
+
+graph_plotter(third_query, new_query, 'RedCard stats')
 
 
+# #for i in range (len(second_processed_data)):
+#     HomeTeam = second_processed_data.Hom
 
-###CREATING AN SQL QUERY TO CREATE A DATA frame | 1. create the query and save to dataframe. 2. create a name for the file. 3. save the file and create a plot 
-
+#PRINT OF RESULTS TO CHECK THAT THE CORRECT DATA HAS COME OUT
+# print(first_processed_data.head())
+# print(second_processed_data.head(), second_processed_data.shape, second_processed_data.size)
 
 #Allows us to close a connection when needed
 cursor.close()
